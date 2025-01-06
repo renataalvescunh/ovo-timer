13cbbf88a229b0a9aab6b6b0af2ff465ba42db86
Renata <renataalvescunh@gmail.com>
2025-01-06 14:04:06 -0300

    arrumando js

--- js/script.js
+++ js/script.js
@@ -9,39 +9,39 @@ function startTimer(minutes) {
     let time = minutes * 60;
     timerElement.textContent = formatTime(time);
 
-    const hoverSound = document.getElementById('hover-sound'); // Usando o mesmo som para o hover e o tic
+    const hoverSound = document.getElementById('hover-sound');
 
     interval = setInterval(() => {
         time--;
         timerElement.textContent = formatTime(time);
 
-        // Toca o som a cada segundo
-        hoverSound.play(); // Reutilizando o som de hover a cada segundo
+       
+        hoverSound.play(); 
 
         if (time <= 0) {
-            clearInterval(interval); // Para o cronômetro
-            timerElement.textContent = 'Tempo terminado!';
-            endSound.play(); // Toca o som de finalização
+            clearInterval(interval);
+            timerElement.textContent = 'Bom trabalho, bom apetite!';
+            endSound.play(); 
         }
     }, 1000);
 }
 
-// Função para formatar o tempo
+//   formatar o tempo
 function formatTime(seconds) {
     const minutes = Math.floor(seconds / 60);
     const remainderSeconds = seconds % 60;
     return `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
 }
 
-// Função para resetar o cronômetro
+//   resetar o cronômetro
 function resetTimer() {
-    clearInterval(interval); // Limpar o intervalo ao reiniciar
+    clearInterval(interval); 
     document.getElementById('hover-btn').style.display = 'block';
     document.getElementById('resetButton').style.display = 'none';
     document.getElementById('timer').textContent = '';
 
-    // Parar o áudio de finalização e resetar para o início
+
     endSound.pause();
-    endSound.currentTime = 0; // Reseta a posição do áudio para o início
+    endSound.currentTime = 0; 
 }
 
